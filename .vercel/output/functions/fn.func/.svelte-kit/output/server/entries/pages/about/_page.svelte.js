8d3f035d61255af2ec69cef7d030aa54c5eccd6d
Jia Jing Loh <jiajing19932@gmail.com>
2024-06-05 01:25:48 +0800

    delete all vercel build file

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/about/_page.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../../chunks/ssr.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div class="container mx-auto flex items-center justify-center h-screen border border-dotted border-zinc-800" data-svelte-h="svelte-6yiaet"><div class="text-center"><h1 class="text-4xl font-bold mb-3">Catfolio is a folio of all kinds of cats</h1> <p>Visit <a class="text-pink-500" href="/">Home</a> to view all cuties</p></div></div>`;
-});
-export {
-  Page as default
-};
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.BON1GQHw.js","_app/immutable/chunks/scheduler.DUa3pFyD.js","_app/immutable/chunks/index.C2NzGguW.js"];
-export const stylesheets = ["_app/immutable/assets/0.Ba7kssZm.css"];
-export const fonts = [];
